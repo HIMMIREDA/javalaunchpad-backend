915a3c8ad6e63ef0ba92520650ae9a8c3ffd2974
hamza nassour <hamza.nassour13@gmail.com>
2023-07-08 22:10:24 +0100

    fixing authentication issues : using and authenticationEntryPoint to prevent the default behavior of spring security (redirection to login page ) and return only 401 if the user is not allowed to perform an action and by intercepting this status in the front end we can make a decision and redirect the user to login page .

--- src/main/java/com/javalaunchpad/security/SecurityConfig.java
+++ src/main/java/com/javalaunchpad/security/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.javalaunchpad.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.HttpBasicDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,20 +33,23 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // Other security configuration beans or methods
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors().configurationSource(corsConfigurationSource());
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.cors(Customizer.withDefaults());
+        httpSecurity.formLogin( httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
+                .successHandler((request, response, authentication) -> response.setStatus(HttpStatus.OK.value()))
+                .failureHandler((request, response, exception) -> response.setStatus(HttpStatus.UNAUTHORIZED.value()))
+        );
         httpSecurity.httpBasic(Customizer.withDefaults());
         httpSecurity.logout().deleteCookies("JSESSIONID").clearAuthentication(true);
-        httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
         httpSecurity
                 .authorizeHttpRequests()
                     .requestMatchers("/h2-console/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
         return httpSecurity.build();
     }
     @Bean
